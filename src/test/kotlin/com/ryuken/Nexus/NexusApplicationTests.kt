843ed9bf48cd55a6c758a62f30f49b3f4dec1492
w0lzard <kpsingj6@gmail.com>
2026-03-09 10:00:00 +0530

    test: update NexusApplicationTests and test configuration

--- src/test/kotlin/com/ryuken/Nexus/NexusApplicationTests.kt
+++ src/test/kotlin/com/ryuken/Nexus/NexusApplicationTests.kt
@@ -1,13 +1,17 @@
 package com.ryuken.Nexus
 
+import com.ryuken.Nexus.config.TestRedisConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestRedisConfig::class)
 class NexusApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
